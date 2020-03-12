30ca55cda0f2da70396fd06b2a9c9e952312de54
James King <james@wearecrystal.uk>
2020-03-12 19:31:56 +0000

    some viewer stuff

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,19 +1,40 @@
 <script>
-export default {};
+export default {
+  props: ['config', 'files'],
+  data: () => ({ current: 0 }),
+
+  computed: {
+    focus() {
+      return this.files.length ? this.files[this.current] : {};
+    },
+    upcomming() {
+      return this.files.slice(this.current, this.current + 20);
+    },
+
+    bg() {
+      return `background:url(file://${this.focus.path})`;
+    },
+  },
+};
 </script>
 
 <template>
   <div class="flex flex-col">
-    <div class="flex-1">
-      Current Image
+    <div class="m-1 flex-1 relative">
+      <img :src="`file://${focus.path}`" class="w-full h-full object-contain absolute inset-0" />
     </div>
-    <div class="flex">
-      <div>
-        Upcomming
-      </div>
-      <div>
-        Previous
-      </div>
+    <div class="flex overflow-hidden">
+      <img
+        :src="`file://${img.path}`"
+        class="h-40 object-contain rounded m-1 block object-cover thing"
+        :class="{ 'border-2': img.id === current }"
+        v-for="img in upcomming"
+        :key="img.path"
+        @click="current = img.id"
+      />
     </div>
+    <!-- <div>{{ current }} of {{ files.length }}</div> -->
   </div>
 </template>
+
+<style lang="postcss" scoped></style>
