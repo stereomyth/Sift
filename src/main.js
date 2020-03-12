e20bc604907af3b841421bf03012db7b0fde30e9
James King <james@wearecrystal.uk>
2020-03-12 12:17:36 +0000

    allow node in render

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ const path = require('path');
 
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { nodeIntegration: true },
+  });
 
   // and load the index.html of the app.
   // mainWindow.loadFile(path.join(__dirname, "index.html"));
