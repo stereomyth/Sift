a551bb40c93b7d6efb4d676090f4bbe5236ae5f4
James King <james@wearecrystal.uk>
2020-03-12 19:31:35 +0000

    folder path interface

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -2,10 +2,18 @@
 import { remote } from 'electron';
 
 export default {
+  props: ['parent', 'source'],
+
+  computed: {
+    parentStyle() {
+      return `width: ${this.parent.length * 0.6 + 0.5}em`;
+    },
+  },
+
   methods: {
     open() {
-      remote.dialog.showOpenDialog().then(thing => {
-        console.log(thing);
+      remote.dialog.showOpenDialog({ properties: ['openDirectory'] }).then(thing => {
+        this.$emit('change', { parent: thing.filePaths[0] });
       });
     },
   },
@@ -13,7 +21,27 @@ export default {
 </script>
 
 <template>
-  <div class="">
-    <button class="bg-black rounded p-2 m-2" @click="open">Open</button>
+  <div>
+    <div class="flex items-center">
+      <button class="bg-black rounded p-2 m-2" @click="open">Open</button>
+      <div>
+        <!-- <label for="">Parent</label> -->
+        <input
+          type="text"
+          :style="parentStyle"
+          :value="parent"
+          @input="$emit('change', { parent: $event.target.value })"
+        />
+      </div>
+      <div class="font-mono">/</div>
+      <div>
+        <!-- <label for="">Source</label> -->
+        <input
+          type="text"
+          :value="source"
+          @input="$emit('change', { source: $event.target.value })"
+        />
+      </div>
+    </div>
   </div>
 </template>
