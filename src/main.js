407341d346ffc9188327aa62604d0a0322724454
James King <james@wearecrystal.uk>
2020-03-12 19:31:07 +0000

    disable websecurity

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: { nodeIntegration: true },
+    webPreferences: { nodeIntegration: true, webSecurity: false },
   });
 
   // and load the index.html of the app.
