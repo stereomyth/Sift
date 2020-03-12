f30f46a7f086d0717696d4d0dbdd6ba0aba53b4f
James King <james@wearecrystal.uk>
2020-03-12 19:31:22 +0000

    folder choices

--- src/components/Side.vue
+++ src/components/Side.vue
@@ -1,5 +1,6 @@
 <script>
 export default {
+  props: ['files'],
   data: () => ({ choices: [1, 2, 3, 4, 5, 6, 7] }),
 };
 </script>
@@ -7,12 +8,8 @@ export default {
 <template>
   <div class="flex bg-red">
     <div class="choices">
-      <button
-        v-for="choice in choices"
-        :key="choice"
-        class="bg-black rounded block w-40 text-left p-2 m-2"
-      >
-        {{ choice }}
+      <button v-for="choice in files" :key="choice.path" class="btn">
+        {{ choice.name }}
       </button>
     </div>
   </div>
