0c61c37acb180fe9f62417cb99f8f08f78aa14b4
James King <james@wearecrystal.uk>
2020-03-12 19:30:53 +0000

    some base styles

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,25 @@ export default {
 
 body {
   background-color: #333;
-  color: white;
+  @apply text-white;
+}
+
+input {
+  @apply font-mono text-white mx-1 px-1;
+  background-color: rgba(255, 255, 255, 0.1);
+}
+
+label {
+  @apply block text-xs;
+}
+
+pre {
+  @apply border-dashed border-2 p-2 m-2;
+  font-size: 10px;
+}
+
+.btn {
+  @apply bg-black rounded block w-40 text-left p-2 m-2;
 }
 
 @tailwind components;
