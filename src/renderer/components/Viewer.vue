e6c026d089b24950528b3a5b910c8e043472436c
James King <james@wearecrystal.uk>
2020-03-13 20:01:15 +0000

    viewer meta

--- src/renderer/components/Viewer.vue
+++ src/renderer/components/Viewer.vue
@@ -20,11 +20,19 @@ export default {
         class="w-full h-full object-contain absolute inset-0"
         v-if="focus.path"
       />
-      <div class="absolute bottom-0 bg-black opacity-25 rounded m-2 p-1 hover:opacity-75">
+      <div class="meta">
         {{ focus.filename }}
       </div>
+      <div class="meta right-0">{{ cursor }} of {{ images.length }}</div>
     </div>
   </div>
 </template>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+.meta {
+  @apply absolute bottom-0 bg-black opacity-25 rounded m-2 p-1 leading-none cursor-default;
+}
+.meta:hover {
+  @apply opacity-75;
+}
+</style>
