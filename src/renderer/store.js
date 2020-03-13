ca3d94305d6c3a51ab8b7d24449c9dd5cfc25bd8
James King <james@wearecrystal.uk>
2020-03-13 18:59:26 +0000

    store dest and source dirreads

--- src/renderer/store.js
+++ src/renderer/store.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+const fs = require('fs');
+const { join } = require('path');
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -17,6 +20,29 @@ export default new Vuex.Store({
       Object.keys(obj).forEach(key => {
         state[key] = obj[key];
       });
+    },
+
+    getDestDirs(state) {
+      if (state.destPath) {
+        fs.readdir(state.destPath, { withFileTypes: true }, (error, items) => {
+          state.destDirs = items
+            .filter(item => item.isDirectory())
+            .map(dir => ({ path: join(state.destPath, dir.name), ...dir }));
+        });
+      }
+    },
+
+    getSrcImgs(state) {
+      // make recursive?
+      if (state.srcPath) {
+        fs.readdir(join(state.srcPath), (error, files) => {
+          state.images = files.map((file, i) => ({
+            path: join(state.srcPath, file),
+            filename: file,
+            id: i,
+          }));
+        });
+      }
     },
   },
 
