8e085e0c07973ac0285bfda036e5d23ca87a8e78
James King <james@wearecrystal.uk>
2020-03-13 18:57:13 +0000

    vuex

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,4 +1,8 @@
 import Vue from 'vue';
-import App from '@/App.vue';
+import App from '@/App';
+import store from '@/store.js';
 
-new Vue({ render: h => h(App) }).$mount('#app');
+new Vue({
+  store,
+  render: h => h(App),
+}).$mount('#app');
