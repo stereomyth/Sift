1541a2a88f2896d9dbf7d5e415e083c582b5db91
James King <james@wearecrystal.uk>
2020-03-13 19:10:21 +0000

    thumbs

---
+++ src/renderer/components/Thumbs.vue
@@ -0,0 +1,32 @@
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    upcomming() {
+      return this.images.slice(this.cursor, this.cursor + 20);
+    },
+
+    ...mapState(['images', 'cursor']),
+  },
+};
+</script>
+
+<template>
+  <div class="flex overflow-hidden">
+    <img
+      :src="`file://${img.path}`"
+      class="h-40 object-contain rounded m-1 block object-cover thing"
+      :class="{ 'border-2': img.id === cursor }"
+      v-for="img in upcomming"
+      :key="img.path"
+      @click="current = img.id"
+    />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.thing {
+  max-width: 160px;
+}
+</style>
