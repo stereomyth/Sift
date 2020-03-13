f28b448daf1429a71fb2d02ccca3daaa13450107
James King <james@wearecrystal.uk>
2020-03-13 19:22:37 +0000

    filename

--- src/renderer/components/Viewer.vue
+++ src/renderer/components/Viewer.vue
@@ -20,6 +20,9 @@ export default {
         class="w-full h-full object-contain absolute inset-0"
         v-if="focus.path"
       />
+      <div class="absolute bottom-0 bg-black opacity-25 rounded m-2 p-1 hover:opacity-75">
+        {{ focus.filename }}
+      </div>
     </div>
   </div>
 </template>
