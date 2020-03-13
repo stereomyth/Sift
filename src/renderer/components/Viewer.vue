429f5e4edfee96ec054a7fbecce9f00ac93fe625
James King <james@wearecrystal.uk>
2020-03-13 19:01:33 +0000

    viewer use store, remove thumbs

--- src/renderer/components/Viewer.vue
+++ src/renderer/components/Viewer.vue
@@ -1,43 +1,28 @@
 <script>
+import { mapState } from 'vuex';
+
 export default {
-  props: ['config', 'files'],
   data: () => ({ current: 0 }),
 
   computed: {
     focus() {
-      return this.files.length ? this.files[this.current] : {};
-    },
-    upcomming() {
-      return this.files.slice(this.current, this.current + 20);
+      return this.images.length ? this.images[this.current] : {};
     },
 
-    bg() {
-      return `background:url(file://${this.focus.path})`;
-    },
+    ...mapState(['images']),
   },
 };
 </script>
 
 <template>
   <div class="flex flex-col">
-    <div class="m-1 flex-1 relative">
+    <div class="m-1 flex-1 relative bg-gray-800 rounded">
       <img
         :src="`file://${focus.path}`"
         class="w-full h-full object-contain absolute inset-0"
         v-if="focus.path"
       />
     </div>
-    <div class="flex overflow-hidden">
-      <img
-        :src="`file://${img.path}`"
-        class="h-40 object-contain rounded m-1 block object-cover thing"
-        :class="{ 'border-2': img.id === current }"
-        v-for="img in upcomming"
-        :key="img.path"
-        @click="current = img.id"
-      />
-    </div>
-    <!-- <div>{{ current }} of {{ files.length }}</div> -->
   </div>
 </template>
 
