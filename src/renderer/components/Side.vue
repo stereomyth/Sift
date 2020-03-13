469f722a5c16f9a54bfe442a0da50a8f7037f214
James King <james@wearecrystal.uk>
2020-03-13 19:22:09 +0000

    basic side

--- src/renderer/components/Side.vue
+++ src/renderer/components/Side.vue
@@ -1,14 +1,21 @@
 <script>
+import { mapState } from 'vuex';
+
 export default {
-  props: ['files'],
-  data: () => ({ choices: [1, 2, 3, 4, 5, 6, 7] }),
+  computed: {
+    choices() {
+      return this.destDirs.filter(d => d.path !== this.destPath);
+    },
+
+    ...mapState(['destDirs', 'destPath']),
+  },
 };
 </script>
 
 <template>
   <div class="flex bg-red">
-    <div class="choices">
-      <button v-for="choice in files" :key="choice.path" class="btn">
+    <div class="choices w-32 flex flex-col">
+      <button v-for="choice in choices" :key="choice.path" class="hover:bg-gray-700 text-left">
         {{ choice.name }}
       </button>
     </div>
