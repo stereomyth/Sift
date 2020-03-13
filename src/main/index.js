defc60c4c449e573431b9dc05b740cbc14de847a
James King <james@wearecrystal.uk>
2020-03-13 18:55:53 +0000

    fix depreciated thing

--- src/main/index.js
+++ src/main/index.js
@@ -28,6 +28,8 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+
+app.allowRendererProcessReuse = true;
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
